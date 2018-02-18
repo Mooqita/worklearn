5bc60e7afd17b25fb8f6b8262b0b9b4336c50df2
Augustin deMayo <gus.demayo@gmail.com>
2018-02-18 11:33:38 -0600

    fix volume mounting

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Cleans Docker Environment For Builds #
+# Clean Environment & Install Dependencies #
 
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker image ls -q)
 docker volume rm $(docker volume ls -q)
+rm -Rf .meteor/local
+rm -Rf node_modules
+npm install
