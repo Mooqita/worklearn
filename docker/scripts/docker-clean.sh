0bff6c9dcefe5f66ffede0e172bd1511a2900978
Augustin deMayo <gus.demayo@gmail.com>
2018-02-18 15:24:21 -0600

    remove settings & environment variables from version control

--- docker/scripts/docker-clean.sh
+++ docker/scripts/docker-clean.sh
@@ -9,3 +9,5 @@ docker volume rm $(docker volume ls -q)
 rm -Rf .meteor/local
 rm -Rf node_modules
 npm install
+cp -n docker/env/.worklearn-env.example docker/env/.worklearn-env
+cp -n settings.json.example settings.json
