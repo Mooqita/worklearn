ac2a7c9c60310df37586152549d19ac6fea57489
Augustin deMayo <gus.demayo@gmail.com>
2018-02-19 21:53:35 -0600

    docker script update v2

--- docker/scripts/docker-clean.sh
+++ docker/scripts/docker-clean.sh
@@ -2,46 +2,54 @@
 
 # Clean Environment & Install Dependencies #
 
-DOCKER_CONTAINERS=($(docker ps -a -q))
-DOCKER_IMAGES=($(docker image ls -q))
-DOCKER_VOLUMES=($(docker volume ls -q))
-
-if [ ${#DOCKER_CONTAINERS[@]} -gt 0 ]; then
-    docker stop $DOCKER_CONTAINERS
-    docker rm $DOCKER_CONTAINERS
-fi
-
-if [ ${#DOCKER_IMAGES[@]} -gt 0 ]; then
-    docker rmi $DOCKER_IMAGES
-fi
-
-if [ ${#DOCKER_VOLUMES[@]} -gt 0 ]; then
-    docker volume rm $DOCKER_VOLUMES
-fi
-
 if [[
     ! -x "$(command -v docker)" ||
     ! -x "$(command -v docker-compose)" 
 ]]; then
+
     echo "You Need To Install Docker & Docker Compose"
+
 fi
 
 if [[
     ! -x "$(command -v node)" ||
     ! -x "$(command -v npm)" 
 ]]; then
+
     echo "You Need To Install Docker & Docker Compose"
+
 fi
 
 if [[
+
     -x "$(command -v docker)" &&
     -x "$(command -v docker-compose)" &&
     -x "$(command -v node)" &&
     -x "$(command -v npm)" 
+
 ]]; then
+
+    DOCKER_CONTAINERS=($(docker ps -a -q))
+    DOCKER_IMAGES=($(docker image ls -q))
+    DOCKER_VOLUMES=($(docker volume ls -q))
+
+    if [ ${#DOCKER_CONTAINERS[@]} -gt 0 ]; then
+        docker stop $DOCKER_CONTAINERS
+        docker rm $DOCKER_CONTAINERS
+    fi
+
+    if [ ${#DOCKER_IMAGES[@]} -gt 0 ]; then
+        docker rmi $DOCKER_IMAGES
+    fi
+
+    if [ ${#DOCKER_VOLUMES[@]} -gt 0 ]; then
+        docker volume rm $DOCKER_VOLUMES
+    fi
+
     npm install
     cp -n docker/env/.worklearn-env.example docker/env/.worklearn-env
     cp -n settings.json.example settings.json
     rm -Rf .meteor/local
     rm -Rf node_modules
+
 fi
